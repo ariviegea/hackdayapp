5ef5399a154ad35eb48048045ca465bd6e1e6df6
ariviegea <ariadna.villar.egea@gmail.com>
2019-11-05 15:28:24 +0100

    created response status from server on the client side

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -17,7 +17,6 @@ const initialState = {
     available: ''
 }
 
-
 class About extends Component {
     constructor(props) {
         super(props);
@@ -29,28 +28,40 @@ class About extends Component {
             [`${e.target.name}`]: e.target.value
         });
     }
-
-    addNewApartment = (e) => {
+    addNewApartment = async(e) => {
+        e.persist()
         e.preventDefault()
         console.log(this.state);
-        fetch('/api/apartments', {
+        const response = await fetch('/api/apartments', {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json'
             },
             body: JSON.stringify(this.state)
         });
-        this.setState(initialState);
-        e.target.reset();
+        if(response.status === 201){
+            this.setState(initialState);
+            e.target.reset()
+            this.displayMessage('Succesful')
+        } else if(response.status === 400){
+            const responseBody = await response.json();
+            this.displayMessage(responseBody.msg);
+        } else {
+            this.displayMessage('Server error')
+        }
     }
 
+    displayMessage = (message) => {
+        console.log(message);
+        return message;
+    }
 
     printInputFields = () => {
         const state = this.state;
         const formInputs = [];
-        for (let key of Object.keys(state)) {
-            if(!(key === 'description')){
-                const inputField = <label style={label}>{key}<input style={input} type='text' name={key} onChange={this.handleEvent} /></label>;
+        for (let property of Object.keys(state)) {
+            if(!(property === 'description')){
+                const inputField = <label style={label}>{property}<input style={input} type='text' name={property} onChange={this.handleEvent} /></label>;
                 formInputs.push(inputField);
             }
         }
